66d367e3b1e92c1cbd7942659a889ba930d7df48
Adrian Castillo <adrianhumbertocastillo@gmail.com>
2021-05-25 20:30:58 -0600

    Readme agregado

---
+++ REDME.md
@@ -0,0 +1,11 @@
+# Notas: 
+Primer programa en Node
+
+```
+Opciones:
+      --help     Muestra ayuda                                          [booleano]
+      --version  Muestra número de versión                              [booleano]
+  -b, --base     Es la base de la tabla de multiplicar                  [número]
+  -h, --hasta    El numero hasta donde llegara la tabla de multiplicar  [número] [defecto: false]
+  -l, --listar   Muestra la tabla en consola                            [booleano] [defecto: false]
+```
--- config/yargs.js
+++ config/yargs.js
@@ -5,14 +5,6 @@ const argv = require('yargs')
         demandOption: false,
         describe: 'Es la base de la tabla de multiplicar'
     })
-    .option('l', {
-        alias: 'listar',
-        type: 'boolean',
-        demandOption: false,
-        default: false,
-        describe: 'Muestra la tabla en consola'
-
-    })
     .option('h', {
         alias: 'hasta',
         type: 'number',
@@ -20,6 +12,14 @@ const argv = require('yargs')
         default: false,
         describe: 'El numero hasta donde llegara la tabla de multiplicar'
     })
+    .option('l', {
+        alias: 'listar',
+        type: 'boolean',
+        demandOption: false,
+        default: false,
+        describe: 'Muestra la tabla en consola'
+
+    })    
     .check( (arg, options) => {
         if(isNaN(arg.b)){
             throw 'La base tiene que ser un numero';
